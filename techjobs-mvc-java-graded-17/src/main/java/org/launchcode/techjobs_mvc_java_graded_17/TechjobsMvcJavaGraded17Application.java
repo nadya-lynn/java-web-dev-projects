9ac1b88fc4c4d9e1cd18962f125dbaeb57808e32
nadya-lynn <154042485+nadya-lynn@users.noreply.github.com>
2024-06-02 12:38:10 -0500

    checking in test

---
+++ techjobs-mvc-java-graded-17/src/main/java/org/launchcode/techjobs_mvc_java_graded_17/TechjobsMvcJavaGraded17Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.techjobs_mvc_java_graded_17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechjobsMvcJavaGraded17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechjobsMvcJavaGraded17Application.class, args);
+	}
+
+}
---
+++ techjobs-mvc-java-graded-17/src/test/java/org/launchcode/techjobs_mvc_java_graded_17/TechjobsMvcJavaGraded17ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.techjobs_mvc_java_graded_17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechjobsMvcJavaGraded17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
